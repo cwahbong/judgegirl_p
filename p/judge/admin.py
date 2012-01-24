3376298a65c757b67a6ce4fc616655108ed21cfb
cwahbong <cw.ahbong@gmail.com>
2012-01-24 22:24:07 +0800

    Minor change
    Add documentation, and make Submission model better.

--- p/judge/admin.py
+++ p/judge/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from p.judge.models import Announcement, Namespace, Link, Problem, Status
+from p.judge.models import Announcement, Namespace, Link, Problem, Status, Submission
 
 
 
@@ -9,10 +9,13 @@ class AnnouncementAdmin(admin.ModelAdmin):
 class StatusAdmin(admin.ModelAdmin):
   list_display = ['description', 'start_time', 'end_time']
 
+class SubmissionAdmin(admin.ModelAdmin):
+  list_display = ['id', 'problem', 'user', 'priority', 'submit_time', 'status', 'result']
 
 admin.site.register(Announcement, AnnouncementAdmin)
 admin.site.register(Link)
 admin.site.register(Namespace)
 admin.site.register(Problem)
 admin.site.register(Status, StatusAdmin)
+admin.site.register(Submission, SubmissionAdmin)
 
--- p/judge/models.py
+++ p/judge/models.py
@@ -5,8 +5,8 @@ from django.core.validators import RegexValidator
 from datetime import datetime
 
 class Announcement(models.Model):
-  """ Represents an announcement.
-      The last modify time will be filled automatically.
+  """ Represents an announcement.  The last modify time will be
+      filled automatically.
   """
   title = models.CharField(max_length=256)
   content = models.TextField()
@@ -20,11 +20,14 @@ class Announcement(models.Model):
     super(Announcement, self).clean(*args, **kwargs)
 
   def __unicode__(self):
-    return self.title + ' (' + str(self.announce_time) + ')'
+    return self.title + ' (' + unicode(self.announce_time) + ')'
 
 
 class Link(models.Model):
-  """ Represents the hyperlink at the 'link' page.
+  """ Represents the hyperlink.
+
+      Note that it will only check the format of the URL.  It will not
+      check if the link is valid.
   """
   name = models.CharField(max_length=256)
   url = models.URLField()
@@ -38,15 +41,18 @@ class Link(models.Model):
 
 
 class Namespace(models.Model):
-  """ Represents an namespace.  Since django can not support
-      multiple column primary key, this model have some issue
-      to solve
+  """ Represents an namespace. The namespace with different parent
+      namespace can have same name.
+
+      The namespace N will be visible when any of its children is visible
+      in order to visit its children (namespace of problem).  It this
+      situation you can only see the visible children of the namespace N.
   """
   parent = models.ForeignKey('self', blank=True, null=True, default='', on_delete=models.CASCADE)
   name = models.CharField(
-    max_length=64,
+    max_length=256,
     validators=[
-      RegexValidator(regex='^\w+$', message='Enter a valid value, it shoule only contain alnum and underscore.'),
+      RegexValidator(regex='^\w+$', message='Enter a valid value, it should only contain alnum and underscore.'),
     ]
   )
 
@@ -58,37 +64,48 @@ class Namespace(models.Model):
     if self.parent==self:
       raise ValidationError('Parent cannot be self.')
     if self.parent==None and Namespace.objects.filter(parent=None).filter(name=self.name):
-      raise ValidationError('Namespace with this Parent and Name already exists.')
+      raise ValidationError('The name of the Namespace with the same Parent already exists.')
     super(Namespace, self).clean(*args, **kwargs)
 
   def __unicode__(self):
     if self.parent and self.parent.name!='':
-      return self.parent.__unicode__() + '::' + self.name
+      return unicode(self.parent) + '::' + self.name
     else:
       return self.name
 
 
 class Problem(models.Model):
+  """ Represents the problem.  The problem can be at exactly one
+      namespace ('None' means the root namespace).
+
+      We suggest you put some problems in the same namespace, then you
+      can change the permission of them together.
+  """
   namespace = models.ForeignKey('Namespace', blank=True, null=True, on_delete=models.PROTECT)
   time_limit = models.IntegerField()          # Unit: second
   memory_limit = models.IntegerField()        # Unit: MB
   output_limit = models.IntegerField()        # Unit: MB
+  input_file = models.CharField(max_length=256, blank=True, null=True, default=None)    # leave blank to use stdin
+  output_file = models.CharField(max_length=256, blank=True, null=True, default=None)   # leave blank to use stdout
   title = models.CharField(max_length=256)
   main_description = models.TextField()
   input_description = models.TextField()
   output_description = models.TextField()
   sample_input = models.TextField()
   sample_output = models.TextField()
-  # input_file = models.TextField()
-  # output_file = models.TextField()
   is_submittable = models.BooleanField(default=True)
-  is_test_uploadable = models.BooleanField()
+  is_test_uploadable = models.BooleanField(default=False)
 
   def __unicode__(self):
-    return self.namespace.__unicode__() + '::' + self.title 
+    if self.namespace:
+      return unicode(self.namespace) + '::' + self.title
+    else:
+      return '::' + self.title
 
 
 class Status(models.Model):
+  """
+  """
   users = models.ManyToManyField(User, blank=True, null=True, default=None)
   groups = models.ManyToManyField(Group, blank=True, null=True, default=None)
   namespaces = models.ManyToManyField('Namespace', blank=True, null=True, default=None)
@@ -112,21 +129,66 @@ class Status(models.Model):
 
 
 class Submission(models.Model):
+  """ under construction
+  """
   problem = models.ForeignKey('Problem', null=True, on_delete=models.SET_NULL)
   user = models.ForeignKey(User)
-  priority = models.IntegerField()
+  priority = models.IntegerField(
+    choices=(
+      (2, 'Highest'),
+      (1, 'High'),
+      (0, 'Normal'),
+      (-1, 'Low'),
+      (-2, 'Lowest'),
+    )
+  )
   code = models.TextField()
-  submit_time = models.DateTimeField()
-  request_time = models.DateTimeField()
-  result = models.CharField(max_length=64)
-  message = models.CharField(max_length=1024)
+  submit_time = models.DateTimeField(editable=False)
+  request_time = models.DateTimeField(editable=False)
+  status = models.CharField(
+    max_length=2,
+    editable=False,
+    choices=(
+      ('NS', 'New submission'),
+      ('JG', 'Judging'),
+      ('RJ', 'Rejudging'),
+      ('JD', 'Judged')
+    ),
+    default='NS'
+  )
+  result = models.CharField(
+    max_length=3,
+    editable=False,
+    choices=(
+      ('AC', 'Accepted'),
+      ('NA', 'Not accepted'),
+      ('WA', 'Wrong answer'),
+      ('TLE', 'Time limit exceeded'),
+      ('MLE', 'Memory limit exceeded'),
+      ('OLE', 'Output limit exceeded'),
+      ('RTE', 'Runtime error'),
+      ('RF', 'Restricted function'),
+      ('CE', 'Compile error'),
+      ('SE', 'System error'),
+      ('--', '--')
+    ),
+    default='--'
+  )
+  result_message = models.TextField(editable=False)
+
+  def clean(self, *args, **kwargs):
+    self.submit_time = datetime.now()
+    self.request_time = self.submit_time
+    super(Submission, self).clean(*args, **kwargs)
 
   def __unicode__(self):
-    return "Problem: " + self.problem + " User: " + self.user + " Time: " + str(self.submit_time)
+    return 'Problem: ' + unicode(self.problem) + " User: " + unicode(self.user) + " Time: " + unicode(self.submit_time)
 
 
 class TestData(models.Model):
-  problem = models.ForeignKey('Problem', null=True)
+  """ under construction
+  """
+  usable_problem = models.ForeignKey('Problem', null=True)
   # input = models.TextField()
   # output = models.TextField()
 
